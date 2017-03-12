d65ec3911827cd102c58d1b174dc706658047d86
adn9x <ducanhk60uet@gmail.com>
2017-03-12 11:34:23 +0700

    update server

--- TeachMeServer/Models/todo.js
+++ TeachMeServer/Models/todo.js
@@ -2,9 +2,9 @@ var connection = require('../connection');
 var mysql = require('mysql');
 
 function Todo() {
-    this.getListPost = function (res) {
+    this.getListPost = function (id,res) {
         connection.acquire(function (err, con) {
-            con.query('select idpost,title,tenmon from post,subject where post.idsubject = subject.idsubject', function (err, result) {
+            con.query('select IDPost,Title,Content,Address,Tienday,Sdt,Tenmon,Post.IDFb,Name from Post,Subject,Account where Account.IDFb = Post.IDFb and Post.IDSubject = Subject.IDSubject and type = ? order by PostTime DESC',id, function (err, result) {
                 con.release();
                 res.send(result);
             });
@@ -13,16 +13,17 @@ function Todo() {
 
     this.getSub = function (res) {
         connection.acquire(function (err, con) {
-            con.query('select * from subject', function (err, result) {
+            con.query('select * from Subject', function (err, result) {
                 con.release();
                 res.send(result);
             });
         });
     };
 
-    this.getPostinSub = function (id, res) {
+
+    this.getPostinSub = function (ten, res) {
         connection.acquire(function (err, con) {
-            con.query('select * from post where IDSubject=?', [id], function (err, result) {
+            con.query('select IDPost,Title,Content,Address,Tienday,Sdt,Tenmon,Post.IDFb,Name from Post,Subject,Account where Account.IDFb = Post.IDFb and Post.IDSubject = Subject.IDSubject and TenMon=?', ten, function (err, result) {
                 con.release();
                 res.send(result);
             });
@@ -31,25 +32,30 @@ function Todo() {
 
     this.getPostAcc = function (id, res) {
         connection.acquire(function (err, con) {
-            con.query('select * from post where IDAccount=?', [id], function (err, result) {
+            con.query('select * from post where IDFb=?', [id], function (err, result) {
                 con.release();
                 res.send(result);
             });
         });
     };
 
-    this.read = function (id, res) {
+    this.getPostBookmark = function (id, res) {
+        var querySQL = "select Post.IDPost,Title,Content,Address,Tienday,Sdt,Tenmon,Post.IDFb,Name from Post,Subject,Account,bookmark where Post.IDSubject = Subject.IDSubject and Account.IDFb = Post.IDFb and Post.IDPost=bookmark.IDPost and bookmark.IDFb =  "+id;
+        console.log(querySQL);
         connection.acquire(function (err, con) {
-            con.query('select title,content,sdt,tienday,tenmon,name,idfb from post,account where post.idaccount=account.idaccount and idpost=?', [id], function (err, result) {
+            con.query(querySQL, function (err, result) {
                 con.release();
                 res.send(result);
             });
         });
     };
 
+    //updated
     this.checkUser = function (idfb, res) {
+        var querySQL = 'SELECT * FROM `Account` WHERE `IDFb` = '+idfb;
         connection.acquire(function (err, con) {
-            con.query('SELECT * FROM `account` WHERE idfb = ?', idfb, function (err, result) {
+            con.query(querySQL, function (err, result) {
+                //console.log(querySQL);
                 con.release();
                 res.send(result);
             });
@@ -57,8 +63,8 @@ function Todo() {
     };
 
     this.createUser = function (field_data, res) {
-        var querySQL = "insert into account(name,email,gender,birthday,idfb) values(?,?,?,?,?)";
-        var table = [field_data.name,field_data.email,field_data.gender,field_data.birthday,field_data.idFB];
+        var querySQL = "insert into Account(Name,IDFb,Email,Gender,Birthday) values(?,?,?,?,?)";
+        var table = [field_data.name,field_data.id,field_data.email,field_data.gender,field_data.birthday];
         querySQL = mysql.format(querySQL,table);
         connection.acquire(function (err, con) {
             con.query(querySQL , function (err, result) {
@@ -73,8 +79,25 @@ function Todo() {
     };
 
     this.createPost = function (field_data, res) {
-        var querySQL = "insert into post values(?,?,?,?,?,?,?,?)";
-        var table = [parseInt(field_data.idAccount),field_data.Title,field_data.PostTime,field_data.Content,field_data.Address,parseInt(field_data.Sdt),field_data.Tienday,parseInt(field_data.type),field_data.IDAccount,field_data.IDSubject];
+        var querySQL = "insert into Post(Title,PostTime,Content,Address,Sdt,Tienday,type,IDFb,IDSubject) values(?,?,?,?,?,?,?,?,?)";
+        var table = [field_data.Title,field_data.PostTime,field_data.Content,field_data.Address,field_data.Sdt,field_data.Tienday,parseInt(field_data.type),field_data.IDFb,field_data.IDSubject];
+        querySQL = mysql.format(querySQL,table);
+        //console.log(querySQL);
+        connection.acquire(function (err, con) {
+            con.query(querySQL , function (err, result) {
+                con.release();
+                if (err) {
+                    res.send({status: 1, message: 'TODO creation failed'});
+                } else {
+                    res.send({status: 0, message: 'TODO created successfully'});
+                }
+            });
+        });
+    };
+
+    this.createBookMark = function (field_data,res) {
+        var querySQL = "insert into bookmark(IDFb,IDPost) values(?,?)";
+        var table = [field_data.IDFb,field_data.IDPost];
         querySQL = mysql.format(querySQL,table);
         connection.acquire(function (err, con) {
             con.query(querySQL , function (err, result) {
@@ -89,11 +112,8 @@ function Todo() {
     };
 
     this.update = function (field_data, res) {
-        var querySQL = "update post set ? where id = ?";
-        var table = [];
-        querySQL = mysql.format(querySQL,table);
         connection.acquire(function (err, con) {
-            con.query(querySQL, function (err, result) {
+            con.query('update todo_list set ? where id = ?', [field_data, field_data.id], function (err, result) {
                 con.release();
                 if (err) {
                     res.send({status: 1, message: 'TODO update failed'});
@@ -104,16 +124,57 @@ function Todo() {
         });
     };
 
-
-    this.delete = function (id, res) {
+    this.read = function (id, res) {
         connection.acquire(function (err, con) {
-            con.query('delete from post where id = ?', [id], function (err, result) {
+            con.query('select title,content,sdt,tienday,tenmon,name,idfb from post,account where post.IDFb=account.IDFb and idpost=?', [id], function (err, result) {
+                con.release();
+                res.send(result);
+            });
+        });
+    };
+
+    this.deleteBookmark = function (field_data, res) {
+        var querySQL = 'delete from bookmark where IDFb = ? and IDPost = ?';
+        var table = [ field_data.IDFb,field_data.IDPost];
+        querySQL = mysql.format(querySQL,table);
+        connection.acquire(function (err, con) {
+            con.query(querySQL, function (err, result) {
+
                 con.release();
                 if (err) {
                     res.send({status: 1, message: 'Failed to delete'});
                 } else {
                     res.send({status: 0, message: 'Deleted successfully'});
                 }
+            });
+        });
+    };
+
+    this.deleteAllBookmark = function (field_data, res) {
+        var querySQL = 'delete from bookmark where IDFb = ? ';
+        var table = [ field_data.IDFb];
+        querySQL = mysql.format(querySQL,table);
+        connection.acquire(function (err, con) {
+            con.query(querySQL, function (err, result) {
+
+                con.release();
+                if (err) {
+                    res.send({status: 1, message: 'Failed to delete'});
+                } else {
+                    res.send({status: 0, message: 'Deleted successfully'});
+                }
+            });
+        });
+    };
+
+    this.checkBookmark = function (IDFb,IDPost, res) {
+        var querySQL = 'select * from bookmark where IDFb = ? and IDPost = ?';
+        var table = [ IDFb,IDPost];
+        querySQL = mysql.format(querySQL,table);
+        connection.acquire(function (err, con) {
+            con.query(querySQL, function (err, result) {
+                con.release();
+                res.send(result);
             });
         });
     };
--- TeachMeServer/routes.js
+++ TeachMeServer/routes.js
@@ -2,28 +2,37 @@ var todo = require('./Models/todo');
 
 module.exports = {
     configure: function (app) {
-        app.get('/todo/getlistpost', function (req, res) {
-            todo.getListPost(res);
+        app.get('/todo/getlistpost/:id/', function (req, res) {
+            todo.getListPost(req.params.id,res);
         });
 
         app.get('/todo/getsubject', function (req, res) {
             todo.getSub(res);
         });
+        app.get('/todo/getpost', function (req, res) {
+            todo.getPost(res);
+        });
 
         app.get('/todo/read/:id/', function (req, res) {
-            todo.read(req.params.id,res);
+            todo.read(res);
         });
 
         app.get('/todo/checkuser/:id/', function (req, res) {
             todo.checkUser(req.params.id,res);
+            console.log(req.params.id);
         });
 
-        app.get('/todo/getpostinsub/:id/', function (req, res) {
-            todo.getPostinSub(req.params.id,res);
+        app.get('/todo/getpostinsub/:ten/', function (req, res) {
+            todo.getPostinSub(req.params.ten, res);
         });
 
         app.get('/todo/getpostacc/:id/', function (req, res) {
             todo.getPostAcc(req.params.id,res);
+        });
+
+
+        app.get('/todo/getpostbookmark/:id/', function (req, res) {
+            todo.getPostBookmark(req.params.id,res);
         });
 
         app.post('/todo/createuser', function (req, res) {
@@ -34,12 +43,25 @@ module.exports = {
             todo.createPost(req.body, res);
         });
 
+        app.post('/todo/createbookmark', function (req, res) {
+            todo.createBookMark(req.body, res);
+        });
+
         app.put('/todo/update', function (req, res) {
             todo.update(req.body, res);
         });
 
-        app.delete('/todo/delete/:id/', function (req, res) {
-            todo.delete(req.params.id, res);
+        app.post('/todo/deletebookmark/', function (req, res) {
+            todo.deleteBookmark(req.body, res);
         });
+
+        app.post('/todo/delallbookmark/', function (req, res) {
+            todo.deleteAllBookmark(req.body, res);
+        });
+
+        app.get('/todo/checkbookmark/:IDFb/:IDPost', function (req, res) {
+            todo.checkBookmark(req.params.IDFb,req.params.IDPost, res);
+        });
+
     }
 };
